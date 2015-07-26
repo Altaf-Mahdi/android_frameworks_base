d807111d03613ba5526d417e19ebcb758e57b384
LorDClockaN <davor@losinj.com>
2015-07-26 14:18:48 +0200

    FWB: Headset icon switch (1/2)
    Change-Id: I561cf99935f9c1ddd0264640dc928bb29d45bdc5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3874,6 +3874,12 @@ public final class Settings {
         public static final String PIE_GRAVITY = "pie_gravity";
 
         /**
+         * Switch for headset icon
+         * @hide
+         */
+        public static final String SHOW_HEADSET_ICON = "show_headset_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
