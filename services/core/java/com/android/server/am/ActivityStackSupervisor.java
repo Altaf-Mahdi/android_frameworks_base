722078ba93bf4d8985fffc76cf0aad1163c4b3d1
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-09-05 23:47:23 +0100

    Revert "power: Add a new power hint for application launches"
    This reverts commit 191527dd0953a8886c1fbe06e87eb07fe66ec8ae.

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -1351,7 +1351,7 @@ public final class ActivityStackSupervisor implements DisplayListener {
                                     container.mActivityDisplay.mDisplayId)));
             Trace.traceBegin(Trace.TRACE_TAG_ACTIVITY_MANAGER , "startActivityLocked");
             /* Acquire perf lock during new app launch */
-            mPm.launchBoost();
+            mPm.cpuBoost(2000 * 1000);
             Trace.traceEnd(Trace.TRACE_TAG_ACTIVITY_MANAGER);
         }
 
@@ -2736,7 +2736,7 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 }
             }
         }
-        mPm.launchBoost();
+        mPm.cpuBoost(2000 * 1000);
 
         /* Delay Binder Explicit GC during application launch */
         BinderInternal.modifyDelayedGcParams();
