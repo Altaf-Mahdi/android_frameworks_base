cc4a6ef163be7df86ca9eba828cfc53b7e3f139d
d34d <clark@cyngn.com>
2016-01-14 10:44:38 -0800

    SysUI: Clean up objects when recreating statusbar
    Recreating the status bar for theme changes causes several views
    and objects to not get GCed.  By analyzing hprof dumps of
    com.android.systemui we can identify these issues and get things
    properly cleaned up, and that's what this patch is doing.
    
    Change-Id: Ieaec759e4378a9a7701e6fba46ed33464caaee0a
    TICKET: CYNGNOS-1041

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -63,6 +63,8 @@ public class KeyguardStatusBarView extends RelativeLayout {
     private int mSystemIconsSwitcherHiddenExpandedMargin;
     private Interpolator mFastOutSlowInInterpolator;
 
+    private UserInfoController mUserInfoController;
+
     private ContentObserver mObserver = new ContentObserver(new Handler()) {
         public void onChange(boolean selfChange, Uri uri) {
             showStatusBarCarrier();
@@ -159,13 +161,17 @@ public class KeyguardStatusBarView extends RelativeLayout {
         mMultiUserSwitch.setUserSwitcherController(controller);
     }
 
+    private UserInfoController.OnUserInfoChangedListener mUserInfoChangedListener =
+            new UserInfoController.OnUserInfoChangedListener() {
+        @Override
+        public void onUserInfoChanged(String name, Drawable picture) {
+            mMultiUserAvatar.setImageDrawable(picture);
+        }
+    };
+
     public void setUserInfoController(UserInfoController userInfoController) {
-        userInfoController.addListener(new UserInfoController.OnUserInfoChangedListener() {
-            @Override
-            public void onUserInfoChanged(String name, Drawable picture) {
-                mMultiUserAvatar.setImageDrawable(picture);
-            }
-        });
+        mUserInfoController = userInfoController;
+        userInfoController.addListener(mUserInfoChangedListener);
     }
 
     public void setKeyguardUserSwitcher(KeyguardUserSwitcher keyguardUserSwitcher) {
@@ -252,7 +258,10 @@ public class KeyguardStatusBarView extends RelativeLayout {
     }
 
     @Override
-    public void onDetachedFromWindow() {
+    protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
+        if (mUserInfoController != null) {
+            mUserInfoController.removeListener(mUserInfoChangedListener);
+        }
     }
 }
