5529471e9a597bceff5d69957d0a01c4bb642ed5
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-03-21 16:22:56 +0000

    QS: add proper logger constants for custom tiles
    Change-Id: I9110881d92ccd657e2b7a09a6891453c78091e22

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BrightnessTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BrightnessTile.java
@@ -91,7 +91,7 @@ public class BrightnessTile extends QSTile<QSTile.BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return CMMetricsLogger.DONT_LOG;
+        return CMMetricsLogger.TILE_BRIGHTNESS;
     }
 
     @Override
--- packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -87,7 +87,7 @@ public class ScreenshotTile extends QSTile<QSTile.BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return CMMetricsLogger.DONT_LOG;
+        return CMMetricsLogger.TILE_SCREENSHOT;
     }
 
     @Override
